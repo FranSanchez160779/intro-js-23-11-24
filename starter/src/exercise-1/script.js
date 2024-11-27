dce743d5eb02b93d724316f64619f60119c927c3
FranSanchez160779 <cixcosanchez@gmail.com>
2024-11-27 18:08:23 +0100

    Delete starter/src directory

--- starter/src/exercise-1/script.js
+++
@@ -1,6 +0,0 @@
-const str1 = prompt("Introduce la primera cadena:");
-const str2 = prompt("Introduce la segunda cadena:");
-const concatenated = str1 + str2;
-const length = concatenated.length;
-
-document.getElementById("output").innerHTML = `<p>La longitud de la cadena concatenada es: ${length}</p>`;
--- starter/src/exercise-2/script.js
+++
@@ -1,4 +0,0 @@
-const celsius = parseFloat(prompt("Introduce los grados Celsius:"));
-const fahrenheit = (celsius * 9/5) + 32;
-
-document.getElementById("output").innerHTML = `<p>${celsius}°C son equivalentes a ${fahrenheit.toFixed(2)}°F.</p>`;
